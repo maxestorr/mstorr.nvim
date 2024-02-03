ce9f483039f39f4da720484f1bf126827a978c68
maxestorr <maxestorr@gmail.com>
2024-02-03 22:38:36 +0000

    neotree config
    tab is 4 space characters
    
    neotree config

--- lua/options.lua
+++ lua/options.lua
@@ -11,6 +11,10 @@ vim.o.relativenumber = true
 vim.o.hlsearch = false
 vim.o.incsearch = true
 
+vim.o.expandtab = true
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
 vim.o.scrolloff = 6
 
 vim.o.ignorecase = true
