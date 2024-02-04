588329a857f2782b927626757cf6eb00e31c1d9c
maxestorr <maxestorr@gmail.com>
2024-02-04 20:59:13 +0000

    refactor telescope config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,25 +1,30 @@
 return {
-  {
-    -- Make vim.ui.select api use telescope by default
-    "nvim-telescope/telescope-ui-select.nvim",
-  },
-  {
-    "nvim-telescope/telescope.nvim",
-    branch = "0.1.x",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      require("telescope").setup({
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown({}),
-          },
+    {
+        "nvim-telescope/telescope.nvim",
+        branch = "0.1.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+            "nvim-tree/nvim-web-devicons",
+            "nvim-telescope/telescope-ui-select.nvim",
         },
-      })
-      local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch for [f]iles" })
-      vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch via [g]rep" })
+        config = function()
+            local telescope = require("telescope")
+            local builtin = require("telescope.builtin")
+            local keymap = vim.keymap
 
-      require("telescope").load_extension("ui-select")
-    end,
-  },
+            telescope.setup({
+                extensions = {
+                    ["ui-select"] = {
+                        require("telescope.themes").get_dropdown({}),
+                    },
+                },
+            })
+            keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch for [f]iles" })
+            keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch via [g]rep" })
+
+            telescope.load_extension("ui-select")
+            telescope.load_extension("fzf")
+        end,
+    },
 }
