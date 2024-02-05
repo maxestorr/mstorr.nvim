fbb035fd41bbe05da509f3a227e2686550749b62
maxestorr <maxestorr@gmail.com>
2024-02-05 23:42:25 +0000

    using dressing as dependency and replacing telescope-ui-select

--- lua/plugins/dressing.lua
+++
@@ -1,5 +0,0 @@
--- This shouldn't be needed because of a telescope plugin
-return {
-    "stevearc/dressing.nvim",
-    event = "VeryLazy",
-}
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,33 +1,25 @@
 return {
-    {
-        "nvim-telescope/telescope.nvim",
-        branch = "0.1.x",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-            "nvim-tree/nvim-web-devicons",
-            "nvim-telescope/telescope-ui-select.nvim",
-            "dhruvasagar/vim-prosession",
-        },
-        config = function()
-            local telescope = require("telescope")
-            local builtin = require("telescope.builtin")
-            local keymap = vim.keymap
+    "nvim-telescope/telescope.nvim",
+    branch = "0.1.x",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+        "nvim-tree/nvim-web-devicons",
 
-            telescope.setup({
-                extensions = {
-                    ["ui-select"] = {
-                        require("telescope.themes").get_dropdown({}),
-                    },
-                },
-            })
-            keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch for [f]iles" })
-            keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch via [g]rep" })
-            keymap.set("n", "<leader>sp", ":Telescope prosession<CR>", { desc = "[S]earch vim [p]rosessions" })
-
-            telescope.load_extension("ui-select")
-            telescope.load_extension("fzf")
-            telescope.load_extension("prosession")
-        end,
+        "dhruvasagar/vim-prosession", -- Search vim sessions
+        "stevearc/dressing.nvim",     -- Make vim.ui-select/input use telescope
     },
+
+    config = function()
+        local telescope = require("telescope")
+        local builtin = require("telescope.builtin")
+        local keymap = vim.keymap
+
+        keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch for [f]iles" })
+        keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch via [g]rep" })
+        keymap.set("n", "<leader>sp", ":Telescope prosession<CR>", { desc = "[S]earch vim [p]rosessions" })
+
+        telescope.load_extension("fzf")
+        telescope.load_extension("prosession")
+    end,
 }
