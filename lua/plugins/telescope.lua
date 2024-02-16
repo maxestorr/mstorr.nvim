2a0f16689eb30ea07c7c4ea1f547fe5112e363e7
maxestorr <maxestorr@gmail.com>
2024-02-16 21:31:34 +0000

    telescope search buffers and man pages

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -42,6 +42,8 @@ return {
 
         keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch for [f]iles" })
         keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch via [g]rep" })
+        keymap.set("n", "<leader>sm", builtin.man_pages, { desc = "[S]earch [m]an pages" })
+        keymap.set("n", "<leader>sb", builtin.buffers, { desc = "[S]earch [b]uffers" })
         keymap.set("n", "<leader>sp", ":Telescope prosession<CR>", { desc = "[S]earch vim [p]rosessions" })
 
         telescope.load_extension("fzf")
