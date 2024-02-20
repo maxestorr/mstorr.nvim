c285800ac2f838801d0297375e1dfaf17fcfea42
maxestorr <maxestorr@gmail.com>
2024-02-20 06:57:50 +0000

    remove nvim-lsp-notify and reduce notification window size

--- lua/plugins/nvim-notify.lua
+++ lua/plugins/nvim-notify.lua
@@ -1,14 +1,19 @@
 -- TODO: I'm meant to set vim.notify = require("notify")
 --      but I'm not sure about the best place to do this yet
 --      this sets vim's default notifications to use nvim-notify
--- TODO: This plugin doesn't seem to work
 return {
     'rcarriga/nvim-notify',
-    dependencies = { 'mrded/nvim-lsp-notify' },
-    init = function()
-        require('notify').setup()
-        require('lsp-notify').setup({
-            notify = require('notify'),
-        })
-    end
+    opts = {
+        timeout = 3000,
+        max_height = function()
+            return math.floor(vim.o.lines * 0.10)
+        end,
+        max_width = function()
+            return math.floor(vim.o.columns * 0.40)
+        end,
+        on_open = function(win)
+            vim.api.nvim_win_set_config(win, { zindex = 100 })
+        end,
+    },
+    config = true,
 }
