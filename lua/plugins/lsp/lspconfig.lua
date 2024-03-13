d0f39cf827466e1c850c38da651371c07f138d3d
maxestorr <maxestorr@gmail.com>
2024-03-13 22:54:10 +0000

    configure java lsp

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -91,5 +91,11 @@ return {
             capabilities = capabilities,
             on_attach = on_attach,
         })
+
+
+        lspconfig.jdtls.setup({
+            capabilities = capabilities,
+            on_attach = on_attach,
+        })
     end,
 }
