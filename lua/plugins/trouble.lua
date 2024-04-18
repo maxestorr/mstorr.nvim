7b106b391374422c179c13eab9d97f18ab8606f9
maxestorr <maxestorr@gmail.com>
2024-04-18 14:19:13 +0100

    trouble v3 including hover lsp info and symbol list

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,18 +1,43 @@
 return {
     "folke/trouble.nvim",
-    cmd = { "TroubleToggle", "Trouble" },
+    branch = "dev",
     dependencies = { "nvim-tree/nvim-web-devicons" },
-    opts = { use_diagnostic_signs = true },
     keys = {
-        { '<leader>xw', '<cmd>TroubleToggle workspace_diagnostics<cr>', desc = 'Trouble [w]orkspace' },
-        { '<leader>xd', '<cmd>TroubleToggle document_diagnostics<cr>', desc = 'Trouble [d]ocument' },
-        { '<leader>xq', '<cmd>TroubleToggle quickfix<cr>', desc = 'Trouble [q]uickfix' },
-        { '<leader>xl', '<cmd>TroubleToggle loclist<cr>', desc = 'Trouble [l]oclist' },
+        {
+            "<leader>xx",
+            "<cmd>Trouble diagnostics toggle<cr>",
+            desc = "Diagnostics (Trouble)",
+        },
+        {
+            "<leader>xX",
+            "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+            desc = "Buffer Diagnostics (Trouble)",
+        },
+        {
+            "<leader>cs",
+            "<cmd>Trouble symbols toggle focus=false<cr>",
+            desc = "Symbols (Trouble)",
+        },
+        {
+            "<leader>cl",
+            "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+            desc = "LSP Definitions / references / ... (Trouble)",
+        },
+        {
+            "<leader>xL",
+            "<cmd>Trouble loclist toggle<cr>",
+            desc = "Location List (Trouble)",
+        },
+        {
+            "<leader>xQ",
+            "<cmd>Trouble qflist toggle<cr>",
+            desc = "Quickfix List (Trouble)",
+        },
         {
             "[q",
             function()
                 if require("trouble").is_open() then
-                    require("trouble").previous({ skip_groups = true, jump = true })
+                    require("trouble").prev({ skip_groups = true, jump = true })
                 else
                     local ok, err = pcall(vim.cmd.cprev)
                     if not ok then
@@ -37,4 +62,5 @@ return {
             desc = "Next trouble/quickfix item",
         },
     },
+    opts = { use_diagnostic_signs = true },
 }
