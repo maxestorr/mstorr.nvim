dbaed2d17094703bc914b4de011301c6ac4cb51d
maxestorr <maxestorr@gmail.com>
2024-06-17 16:47:03 +0100

    issue with markdown formatting plugin

---
+++ lua/plugins/markdown-nvim.lua
@@ -0,0 +1,7 @@
+return {
+    -- fix: terminal isn't currently displaying bold and italic text
+    -- 'MeanderingProgrammer/markdown.nvim',
+    -- -- name = 'render-markdown', -- Only needed if you have another plugin named markdown.nvim
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter' },
+    -- config = true,
+}
