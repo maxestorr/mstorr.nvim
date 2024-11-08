fd114580227a64e2418577cfc2eecc2dc7e6e664
maxestorr <maxestorr@gmail.com>
2024-11-08 14:03:23 +0000

    include file path in lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,16 +1,37 @@
 return {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons', 'folke/noice.nvim' },
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons", "folke/noice.nvim" },
 
-    config = function ()
-        local lualine = require('lualine')
-        local noice = require('noice')
-        local lazy_status = require('lazy.status')
+    config = function()
+        local lualine = require("lualine")
+        local noice = require("noice")
+        local lazy_status = require("lazy.status")
 
         -- Display message in statusline when recording macro
         -- because noice replaces messages ui we need to forward messages to lualine
         lualine.setup({
             sections = {
+                lualine_c = {
+                    {
+                        "filename",
+                        file_status = true, -- Displays file status (readonly status, modified status)
+                        newfile_status = false, -- Display new file status (new file means no write after created)
+                        path = 1, -- 0: Just the filename
+                        -- 1: Relative path
+                        -- 2: Absolute path
+                        -- 3: Absolute path, with tilde as the home directory
+                        -- 4: Filename and parent dir, with tilde as the home directory
+
+                        shorting_target = 40, -- Shortens path to leave 40 spaces in the window
+                        -- for other components. (terrible name, any suggestions?)
+                        symbols = {
+                            modified = "[+]", -- Text to show when the file is modified.
+                            readonly = "[-]", -- Text to show when the file is non-modifiable or readonly.
+                            unnamed = "[No Name]", -- Text to show for unnamed buffers.
+                            newfile = "[New]", -- Text to show for newly created file before first write
+                        },
+                    },
+                },
                 lualine_x = {
                     {
                         noice.api.statusline.mode.get,
@@ -25,5 +46,5 @@ return {
                 },
             },
         })
-    end
+    end,
 }
