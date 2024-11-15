c8925de0a67ad013ce342effbaceb04ba728bcc6
maxestorr <maxestorr@gmail.com>
2024-11-15 23:49:26 +0000

    moved lspconfig into mason using setup_handlers

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -4,52 +4,128 @@ return {
         "williamboman/mason-lspconfig.nvim",
         "jay-babu/mason-null-ls.nvim",
         { "nvimtools/none-ls.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
+        { "neovim/nvim-lspconfig", dependencies = { "hrsh7th/cmp-nvim-lsp" } },
     },
+    event = { "BufReadPre", "BufNewFile" },
 
     config = function()
+        local lspconfig = require("lspconfig")
+        local cmp_nvim_lsp = require("cmp_nvim_lsp")
+
+        local keymap = vim.keymap
+        local opts = { noremap = true, silent = true }
+        local capabilities = cmp_nvim_lsp.default_capabilities()
+
+        local on_attach = function(client, bufnr)
+            opts.buffer = bufnr
+
+            keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+            keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+            keymap.set("n", "K", vim.lsp.buf.hover, opts)
+            -- keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+            keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+            keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
+            keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
+            keymap.set("n", "<space>wl", function()
+                print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+            end, opts)
+            keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
+            keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
+            keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
+            keymap.set("n", "gr", vim.lsp.buf.references, opts)
+
+            keymap.set("n", "<space>f", function()
+                local null_ls_sources = require("null-ls.sources")
+                local ft = vim.bo[bufnr].filetype
+                local has_null_ls = #null_ls_sources.get_available(ft, "NULL_LS_FORMATTING") > 0
+
+                vim.lsp.buf.format({
+                    bufnr = bufnr,
+                    filter = function(client)
+                        if has_null_ls then
+                            return client.name == "null-ls"
+                        else
+                            return true
+                        end
+                    end,
+                })
+            end, opts)
+        end
+
         require("mason").setup({
             ui = {
                 icons = {
                     package_installed = "✓",
                     package_pending = "➜",
-                    package_uninstalled = "✗"
-                }
-            }
+                    package_uninstalled = "✗",
+                },
+            },
         })
 
-        -- TODO: look into :h mason-lspconfig-automatic-server-setup
-        -- I believe this should remove the need for lspconfig.server.setup
-        -- lines in ./lspconfig.lua
-        require("mason-lspconfig").setup {
+        require("mason-lspconfig").setup({
             ensure_installed = {
                 "lua_ls",
-                "bashls",
-                "jsonls",
                 "pyright",
+                "ts_ls",
+                "bashls",
                 "sqlls",
+                "jsonls",
                 "yamlls",
                 "html",
                 "cssls",
-                "ts_ls",
                 "terraformls",
+                "dockerls",
             },
             automatic_installation = true,
-        }
+        })
 
         require("mason-null-ls").setup({
             ensure_installed = {
                 "stylua",
                 "isort",
                 "black",
+                "yamlfmt",
             },
             automatic_installation = true,
             handlers = {},
         })
 
+        require("mason-lspconfig").setup_handlers({
+            -- default handler
+            function(server_name)
+                require("lspconfig")[server_name].setup({
+                    capabilities = capabilities,
+                    on_attach = on_attach,
+                })
+            end,
+
+            -- custom handlers
+            ["lua_ls"] = function()
+                lspconfig.lua_ls.setup({
+                    capabilities = capabilities,
+                    on_attach = on_attach,
+                    settings = {
+                        Lua = {
+                            diagonstics = {
+                                globals = { "vim" },
+                            },
+                            workspace = {
+                                checkThirdParty = false,
+                                library = {
+                                    [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+                                    [vim.fn.stdpath("config") .. "/lua"] = true,
+                                },
+                            },
+                        },
+                    },
+                })
+            end,
+        })
+
         require("null-ls").setup({
             sources = {
-                -- Anything not supported by mason.
-            }
+                -- anything not supported by mason
+            },
         })
     end,
 }
