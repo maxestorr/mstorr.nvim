e26ec1ff8188dbcb9f7d95a2346d91572f79c2c1
Max Storr-Vijay <maxestorr@gmail.com>
2024-11-27 17:19:29 +0000

    added treesitter context plugin

---
+++ lua/plugins/treesitter-context.lua
@@ -0,0 +1,13 @@
+return {
+    "nvim-treesitter/nvim-treesitter-context",
+    opts = {
+        enable = true,
+        multiwindow = false,
+        max_lines = 1,
+        min_window_height = 0,
+        line_numbers = true,
+        multiline_threshold = 20,
+        trim_scope = "outer",
+        mode = "cursor",
+    },
+}
