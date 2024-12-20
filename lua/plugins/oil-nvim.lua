7fe499aeb84105f0a7789acba6eec939553426ba
Max Storr-Vijay <maxestorr@gmail.com>
2024-12-20 12:04:15 +0000

    add oil and change neo-tree to float

---
+++ lua/plugins/oil-nvim.lua
@@ -0,0 +1,12 @@
+return {
+    "stevearc/oil.nvim",
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    -- Optional dependencies
+    dependencies = { { "echasnovski/mini.icons", opts = {} } },
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+    keys = {
+        { "-", ":Oil --float<cr>", desc = "Open current directory in oil" }
+    }
+}
