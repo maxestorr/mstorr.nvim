de58941d9750ffbf2189b1fdda0657279fe4b973
Max Storr-Vijay <maxestorr@gmail.com>
2024-12-23 14:30:30 +0000

    show hidden files in oil

--- lua/plugins/oil-nvim.lua
+++ lua/plugins/oil-nvim.lua
@@ -3,10 +3,16 @@ return {
     ---@module 'oil'
     ---@type oil.SetupOpts
     opts = {},
-    -- Optional dependencies
     dependencies = { { "echasnovski/mini.icons", opts = {} } },
-    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
     keys = {
-        { "-", ":Oil --float<cr>", desc = "Open current directory in oil" }
-    }
+        { "-", ":Oil --float<cr>", desc = "Open current directory in oil" },
+    },
+
+    config = function()
+        require("oil").setup({
+            view_options = {
+                show_hidden = true,
+            },
+        })
+    end,
 }
