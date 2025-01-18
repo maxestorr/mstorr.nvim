3a473d953910dc48c008fab28788dccf135ba31d
Max Storr-Vijay <maxestorr@gmail.com>
2025-01-18 16:32:51 +0000

    remove markdown-nvim

--- lua/plugins/markdown-nvim.lua
+++
@@ -1,7 +0,0 @@
-return {
-    -- fix: terminal isn't currently displaying bold and italic text
-    -- 'MeanderingProgrammer/markdown.nvim',
-    -- -- name = 'render-markdown', -- Only needed if you have another plugin named markdown.nvim
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter' },
-    -- config = true,
-}
