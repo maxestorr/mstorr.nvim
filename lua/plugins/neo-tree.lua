d109e59a8fc17b29696a519c7dccfe7a2db58a73
Max Storr-Vijay <maxestorr@gmail.com>
2025-08-18 15:45:51 +0100

    temporarily disable broken plugins

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,24 +1,24 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-tree/nvim-web-devicons",
-        "MunifTanjim/nui.nvim",
-    },
-    keys = {
-        { "<leader>e", ":Neotree toggle position=float<CR>", desc = "Toggle filetree [e]xplorer", silent = true },
-    },
-    opts = {
-        filesystem = {
-            filtered_items = {
-                visible = true,
-                show_hidden_count = true,
-                hide_dotfiles = true,
-                hide_gitignored = true,
-                hide_by_name = {},
-                never_show = {},
-            },
-        },
-    },
+    -- "nvim-neo-tree/neo-tree.nvim",
+    -- branch = "v3.x",
+    -- dependencies = {
+    --     "nvim-lua/plenary.nvim",
+    --     "nvim-tree/nvim-web-devicons",
+    --     "MunifTanjim/nui.nvim",
+    -- },
+    -- keys = {
+    --     { "<leader>e", ":Neotree toggle position=float<CR>", desc = "Toggle filetree [e]xplorer", silent = true },
+    -- },
+    -- opts = {
+    --     filesystem = {
+    --         filtered_items = {
+    --             visible = true,
+    --             show_hidden_count = true,
+    --             hide_dotfiles = true,
+    --             hide_gitignored = true,
+    --             hide_by_name = {},
+    --             never_show = {},
+    --         },
+    --     },
+    -- },
 }
--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,28 +1,28 @@
 return {
-    -- TODO: Look into snack.notify
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    dependencies = {
-        "MunifTanjim/nui.nvim",
-        "rcarriga/nvim-notify",
-    },
-
-    config = function()
-        require("noice").setup({
-            lsp = {
-                -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-                override = {
-                    ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-                    ["vim.lsp.util.stylize_markdown"] = true,
-                },
-            },
-            -- you can enable a preset for easier configuration
-            presets = {
-                bottom_search = true,
-                command_palette = true,
-                long_message_to_split = true,
-                lsp_doc_border = true,
-            },
-        })
-    end,
+    -- -- TODO: Look into snack.notify
+    -- "folke/noice.nvim",
+    -- event = "VeryLazy",
+    -- dependencies = {
+    --     "MunifTanjim/nui.nvim",
+    --     "rcarriga/nvim-notify",
+    -- },
+    --
+    -- config = function()
+    --     require("noice").setup({
+    --         lsp = {
+    --             -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+    --             override = {
+    --                 ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+    --                 ["vim.lsp.util.stylize_markdown"] = true,
+    --             },
+    --         },
+    --         -- you can enable a preset for easier configuration
+    --         presets = {
+    --             bottom_search = true,
+    --             command_palette = true,
+    --             long_message_to_split = true,
+    --             lsp_doc_border = true,
+    --         },
+    --     })
+    -- end,
 }
